ee7c8601b32d5d944894ce4a27bfab0fad9b8326
mickeygray <mgray@nattaxexperts.com>
2020-09-23 09:27:21 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import SSI from "./components/SSI";
+
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        <Fragment>
+          <Switch>
+            <Route exact path='/' component={SSI} />
+          </Switch>
+        </Fragment>
+      </Router>
     </div>
   );
 }
